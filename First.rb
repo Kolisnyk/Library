340604d8152b9b67ab8fffa5ccc8d3f549e2d095
Oleksii Kolisnyk <Kolisnyk@inbox.ru>
2016-09-28 16:29:09 +0200

    Add files via upload

---
+++ First.rb
@@ -0,0 +1,15 @@
+class First
+    def main ()
+    mass = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,18, 19, 20]
+    boolean marker = false
+    int i = 0
+  
+    while marker == false
+    i++
+    if (((i%mass[0])==0) && ((i%mass[1])==0) && ((i%mass[2])==0) && ((i%mass[3])==0 )&& ((i%mass[4])==0) && ((i%mass[5])==0) && ((i%mass[6])==0) && ((i%mass[7])==0) && ((i%mass[8])==0) && ((i%mass[9])==0) && ((i%mass[10])==0) && ((i%mass[11])==0) && ((i%mass[12])==0) && ((i%mass[13])==0) && ((i%mass[14])==0) && ((i%mass[15])==0) && ((i%mass[16])==0) && ((i%mass[17])==0)&&((i%mass[18])==0) && ((i%mass[19])==0))
+    marker = true
+    puts(i)
+    end
+    end
+    end
+    
